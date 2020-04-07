68bf0d86c0d42b7929bd4af5479b798c6ecec58d
tuantuan <realxym@163.com>
2020-04-07 10:39:02 +0800

    框架搭建

---
+++ src/main/java/com/edu/bidlab/weshareiotservice/WeshareiotserviceApplication.java
@@ -0,0 +1,13 @@
+package com.edu.bidlab.weshareiotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshareiotserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeshareiotserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/bidlab/weshareiotservice/WeshareiotserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bidlab.weshareiotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeshareiotserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
